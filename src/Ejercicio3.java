950372f42c57ee40160a5b84be348582a159281e
Maximiliano <leonico@gmail.com>
2026-03-22 01:13:40 -0300

    .

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
     static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Ingrese el año actual: ");
 
+        int FechaActual = leer.nextInt();
+        System.out.print("Ingrese el año de nacimiento: ");
+        int FechaNac = leer.nextInt();
+
+        System.out.println();
+        System.out.println("Edad: " + (FechaActual-FechaNac));
     }
 }
